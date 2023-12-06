85980a92da16b843dba74634e5ed798e814566f4
Domen Bencak <domen.bencak@student.um.si>
2023-12-06 21:33:11 +0100

    Setup of Dockerfile and project

---
+++ RGIS_IvancicBencak/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RGIS_IvancicBencak
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // Comment or remove the following line to disable HSTS
+                // app.UseHsts();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            // Start the application with a specific port
+            app.Run("http://0.0.0.0:443");
+        }
+    }
+}
