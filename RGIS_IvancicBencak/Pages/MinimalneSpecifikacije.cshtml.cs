0ac8a02742125d827a88ec8f34f6b0592ee60d28
lukaivancic14 <110102950+lukaivancic14@users.noreply.github.com>
2024-01-16 18:21:25 +0100

    Strani - Drugi

--- RGIS_IvancicBencak/Pages/MinimalneSpecifikacije.cshtml.cs
+++ RGIS_IvancicBencak/Pages/MinimalneSpecifikacije.cshtml.cs
@@ -1,20 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RGIS_IvancicBencak.Pages;
 
-namespace RGIS_IvancicBencak.Pages;
-
-public class MinimalneSpecifikacijeModel : PageModel
+namespace RGIS_IvancicBencak
 {
-    private readonly ILogger<IndexModel> _logger;
-
-    public MinimalneSpecifikacijeModel(ILogger<IndexModel> logger)
+    public class MinimalneSpecifikacijeModel : PageModel
     {
-        _logger = logger;
-    }
+        private readonly ILogger<MinimalneSpecifikacijeModel> _logger;
 
-    public void OnGet()
-    {
-
+        public MinimalneSpecifikacijeModel(ILogger<MinimalneSpecifikacijeModel> logger)
+        {
+            _logger = logger;
+        }
+        public void OnGet()
+        {
+        }
     }
 }
-
